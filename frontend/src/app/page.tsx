8a65900bc4da4475760f7597a2cd07968142a77b
Michael Bathke <mbathke666@gmail.com>
2024-12-17 15:59:57 +0100

    [feat-1] Initial commit of frontend (nextjs) and backend (strapijs)

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,23 @@
+async function getStrapiData(path: string) {
+  const baseUrl = 'http://localhost:1337'
+
+  try {
+    const response = await fetch(baseUrl + path)
+    const data = await response.json()
+    return data
+  } catch(e) {
+    console.error(e) 
+  }
+}
+
+export default async function Home() { 
+  const data = await getStrapiData('/api/home-page')
+  const {title, description} = data.data
+
+  return (
+    <main className="container mx-auto py-6"> 
+      <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
+    </main>
+  )
+}
